6b1bf0e619a094a011316cdda051a068592c2826
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-05-03 01:15:07 +0700

    Update linkedin.py

--- linkedin.py
+++ linkedin.py
@@ -1,3 +1,4 @@
+# Nhập thư viện và gói cho dự án
 import csv
 import parameters
 from parsel import Selector
@@ -6,56 +7,63 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-# Operating in headless mode
+# Mở Firefox và truy cập trang đăng nhập Linkedin
 driver = webdriver.Firefox()
 url = 'https://www.linkedin.com'
 driver.get(url)
 sleep(3)
 
+# Nhập tên người dùng (email) và mật khẩu
 credential = open('credentials.txt')
 line = credential.readlines()
 email = line[0]
 password = line[1]
 sleep(2)
 
-# locate email form by_class_name
+# email
 email_field = driver.find_element_by_id('session_key')
-# send_keys() to simulate key strokes
+
+# send_keys () để mô phỏng các thao tác gõ phím
 email_field.send_keys(email)
 sleep(2)
 
-# locate password form by_class_name
+# password 
 password_field = driver.find_element_by_id('session_password')
-# send_keys() to simulate key strokes
+
+# send_keys () để mô phỏng các thao tác gõ phím
 password_field.send_keys(password)
 sleep(2)
 
-# locate submit button by_xpath
+# xác định vị trí nút gửi đăng nhập
 log_in_button = driver.find_element_by_class_name('sign-in-form__submit-button')
 
-# .click() to mimic button click
+# .click () để bắt chước nhấp vào nút đăng nhập
 log_in_button.click()
 sleep(3)
 
 
-# driver.get method() will navigate to a page given by the URL address
+# phương thức driver.get () sẽ điều hướng đến một trang được cung cấp bởi địa chỉ URL
 driver.get('https://www.google.com')
 sleep(3)
 
-# locate search form by_name
+# Nhập từ khóa tìm kiếm
 search = open('search.txt')
 line = search.readlines()
-search_query = line[0]
+skill = line[0]
+local = line[1]
+page_number = line[2]
+keyword = f'site:linkedin.com/in/ AND "{skill}" AND "{local}"'
 search_query = driver.find_element_by_name('q')
 
-# send_keys() to simulate the search text key strokes
-search_query.send_keys(search_query)
+# send_keys () để mô phỏng các thao tác gõ phím
+search_query.send_keys(keyword)
 sleep(3)
 
-# .send_keys() to simulate the return key
+# .send_keys () để mô phỏng khóa trả về
 search_query.send_keys(Keys.RETURN)
 sleep(3)
 
+# thu thập thông tin URL của các cấu hình. viết một hàm để trích xuất các URL của một trang
 def GetURL():
     page_source = BeautifulSoup(driver.page_source, 'html.parser')
     sleep(3)
@@ -65,13 +73,14 @@ def GetURL():
     for profile in profiles:
         for a in profile.find_all('a'):
             sleep(3)
-            profile_URL = a.get('href')
+            profile_URL = a.get('href').replace('vn.','')
             if profile_URL not in all_profile_URL:
                 all_profile_URL.append(profile_URL)
     return all_profile_URL
 
+# Điều hướng qua nhiều trang và trích xuất URL hồ sơ của mỗi trang
 URLs_all_page = []
-for page in range(10):
+for page in range(int(page_number)):
     URLs_one_page = GetURL()
     sleep(3)
     driver.execute_script('window.scrollTo(0, document.body.scrollHeight);') #scroll to the end of the page
@@ -82,33 +91,35 @@ for page in range(10):
     URLs_all_page = URLs_all_page + URLs_one_page
     sleep(3)
 
-with open('output.csv', 'w',  newline = '') as file_output:
-    headers = ['Name', 'Job Title', 'Location', 'URL', 'Company']
+# thu thập dữ liệu của 1 hồ sơ Linkedin và ghi dữ liệu vào tệp .CSV
+with open('profile_output.csv', 'w',  newline = '') as file_output:
+    headers = ['Name', 'Job Title', 'Location', 'Company', 'Link']
     writer = csv.DictWriter(file_output, delimiter=',', lineterminator='\n',fieldnames=headers)
     writer.writeheader()
     for linkedin_URL in URLs_all_page:
         driver.get(linkedin_URL)
-        print('- Accessing profile: ', linkedin_URL)
+        print('- Truy cập hồ sơ: ', linkedin_URL)
         sleep(3)
         page_source = BeautifulSoup(driver.page_source, "html.parser")
         info_div = page_source.find('div',{'class':'flex-1 mr5 pv-top-card__list-container'})
         info_company = page_source.find('ul',{'class':'pv-top-card--experience-list'})
         try:
             name = info_div.find('li', class_='inline t-24 t-black t-normal break-words').get_text().strip() #Remove unnecessary characters 
-            print('--- Profile name is: ', name)
+            print('--- Tên hồ sơ là: ', name)
 
             location = info_div.find('li', class_='t-16 t-black t-normal inline-block').get_text().strip() #Remove unnecessary characters 
-            print('--- Profile location is: ', location)
+            print('--- Vị trí hồ sơ là: ', location)
 
             title = info_div.find('h2', class_='mt1 t-18 t-black t-normal break-words').get_text().strip()
-            print('--- Profile title is: ', title)
+            print('--- Tiêu đề hồ sơ là: ', title)
 
-            company = info_div2.find('span', class_='text-align-left ml2 t-14 t-black t-bold full-width lt-line-clamp lt-line-clamp--multi-line ember-view').get_text().strip()
-            print('--- Profile company is: ', company)
+            company = info_company.find('span', class_='text-align-left ml2 t-14 t-black t-bold full-width lt-line-clamp lt-line-clamp--multi-line ember-view').get_text().strip()
+            print('--- Hồ sơ công ty là: ', company)
 
             writer.writerow({headers[0]:name, headers[1]:title, headers[2]:location, headers[3]:company, headers[4]:linkedin_URL})
             print('\n')
         except:
             pass
 
-print('Mission Completed!')
+# nhiệm vụ hoàn thành
+print('nhiệm vụ hoàn thành!')
